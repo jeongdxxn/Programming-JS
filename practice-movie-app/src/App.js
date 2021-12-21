12f19b86f6adf4216eda7be4e15015196018f6d9
jeongdxxn <dxxn.jeong@gmail.com>
2021-12-21 23:57:04 +0900

    to do

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-
-  useEffect(() => {
-    console.log('Hi :>');
-    return () => {
-      console.log('bye :<');
-    }
-  }, [])
-  return <h1>Hello</h1>
-}
-
 function App() {
   
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false)
+      })
+  }, [])
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null} 
+      <ul>
+        {coins.map((coin) => (
+          <li>{coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD</li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ react-todo-list/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(toDo === ''){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo('');
+  }
+  console.log(toDos);
+
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder="Write your to do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </div>
+  )
+}
+
+export default App
