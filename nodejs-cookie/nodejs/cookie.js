ffbe5f1efaf03364844b9659a8c4339e1c30e90c
jeongdxxn <dxxn.jeong@gmail.com>
2022-01-24 23:43:17 +0900

    cookie 인증

---
+++ nodejs-cookie/nodejs/cookie.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const cookie = require('cookie'); // npm install cookie
+
+http.createServer(function(req, res){
+  // console.log(req.headers.cookie);
+  // ? parse 를 이용해 전달받은 cookie 를 객체화하여 사용하기 편리하게 만든다
+  // ? parse 는 undefined 를 받지 못하기 때문에 if문을 이용해 예외처리를 해준다
+  let cookies = {};
+  if(req.headers.cookie !== undefined){
+    cookies = cookie.parse(req.headers.cookie);
+  }
+  // console.log(cookies.yummy_cookie);
+  // ? 쿠키 생성 방법
+  // ? 복수의 쿠키를 생성할 때는 배열로 작성한다
+  res.writeHead(200, {
+    'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+  })
+  // ? Set-Cookie 로 작성된 쿠키는 웹 브라우저가 리로드를 할때마다 Cookie 라고 하는 헤더값을 통해서 서버로 전송한다
+  // ? Network > Cookies 를 통해 저장된 cookie를 확인하거나, 
+  // ? Appication > 저장용량 > 쿠키 > 로컬주소 에 들어가 저장된 쿠키를 확인 및 삭제할 수 있다
+  res.end('Cookie!');
+}).listen(3000);
