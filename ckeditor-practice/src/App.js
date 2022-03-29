06ec65b23cb95c9fe932c44e54baf6f3bcd9cae7
jeongdxxn <dxxn.jeong@gmail.com>
2022-03-29 14:57:54 +0900

    ckeditor-practice

---
+++ ckeditor-practice/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+`;
+
+const Editor = styled.div`
+  width: 1000px;
+  height: 500px;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Editor>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log(data);
+          }}
+        />
+      </Editor>
+    </Container>
+  );
+}
+
+export default App;
