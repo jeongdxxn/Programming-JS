07a98e68ada13005834c99518e417b5b30442f76
jeongdxxn <dxxn.jeong@gmail.com>
2022-04-17 22:02:50 +0900

    react-select pratice

--- quill-practice/src/Editor.js
+++ quill-practice/src/Editor.js
@@ -1,15 +1,15 @@
-import "./Editor.css";
-import React from "react";
+import React, { useMemo } from "react";
 import { useState } from "react";
-import ReactQuill from "react-quill";
-// import QuillTollbar, { modules, formats } from "./EditorToolbar";
-import "react-quill/dist/quill.snow.css";
+import Select from 'react-select';
 
-import { Quill } from "react-quill";
+// import QuillTollbar, { modules, formats } from "./EditorToolbar";
+import ReactQuill, { Quill } from "react-quill";
 import ImageResize from "quill-image-resize";
+import "./Editor.css";
+import "react-quill/dist/quill.snow.css";
 Quill.register("modules/ImageResize", ImageResize);
 
-//* 툴바 모듈 *//
+//* Quill Toolbar modules *//
 const modules = {
   toolbar: [
     [{ 'font': []}],
@@ -31,25 +31,60 @@ const modules = {
   },
 };
 
+
+// * Selet Component *//
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+
 function Editor() {
   const [state, setState] = useState({ value: null });
   const handleChange = (value) => {
     setState({ value });
   };
+
+const customStyles = useMemo(
+  () => ({
+    option: (provided, state) => ({
+      ...provided,
+      border: "1px dotted black",
+      color: state.data.color,
+      opacity: 0.8,
+      padding: 20,
+    }),
+    control: (provided) => ({
+      ...provided,
+      width: 500,
+      background: "red",
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: state.data.color,
+    }),
+  }),
+  []
+);
+
   return (
-    <div className="editor">
-      <div className="text-editor">
-        {/* <QuillTollbar /> */}
-        <ReactQuill
-          theme="snow"
-          value={state.value}
-          onChange={handleChange}
-          placeholder={"Write something awesome.."}
-          modules={modules}
-          // formats={formats}
-        />
+    <>
+      <Select options={options} styles={customStyles} />
+      <div className="editor">
+        <div className="text-editor">
+          {/* <QuillTollbar /> */}
+          <ReactQuill
+            theme="snow"
+            value={state.value}
+            onChange={handleChange}
+            placeholder={"Write something awesome.."}
+            modules={modules}
+            // formats={formats}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
